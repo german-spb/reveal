1dd9709a0afdf7b7f2757b56cda8347405d8bb2e
Герман <108299345+german-spb@users.noreply.github.com>
2023-09-25 12:35:47 +0300

    Update task.js

--- task.js
+++ task.js
@@ -2,12 +2,12 @@ window.addEventListener('scroll', function(){
     const block = document.querySelectorAll('.reveal')
     for (let i of block) {
         const {top, bottom} = i.getBoundingClientRect()
-            if (bottom > 0) {
+            if (bottom < window.innerHeight) {
                 i.classList.add('reveal_active')
             }
-            if (top < window.innerHeight){
-                i.classList.add('reveal_active')
+            else {
+                i.classList.remove('reveal_active')
             }
-    }
+        }
 }
 )
